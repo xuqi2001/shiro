79b0aa3eb8ac7f4202ac5456587ee6759f568fa0
xuqi2001 <104254715+xuqi2001@users.noreply.github.com>
2022-04-25 14:24:56 +0800

    Add files via upload

---
+++ 0422/3120190971211-徐琦/xihua_demo/src/main/java/com/zlt/entity/User.java
@@ -0,0 +1,168 @@
+package com.zlt.entity;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String nickname;
+    private String mobile;
+    private String email;
+    private String birthday;
+    private int gender;
+    private String avatar;
+    private String user_type;
+    private String reg_ip;
+    private String last_login_ip;
+    private String last_login_time;
+    private String remark;
+    private int login_count;
+    private int status;
+    private String create_time;
+    private String update_time;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getUser_type() {
+        return user_type;
+    }
+
+    public void setUser_type(String user_type) {
+        this.user_type = user_type;
+    }
+
+    public String getReg_ip() {
+        return reg_ip;
+    }
+
+    public void setReg_ip(String reg_ip) {
+        this.reg_ip = reg_ip;
+    }
+
+    public String getLast_login_ip() {
+        return last_login_ip;
+    }
+
+    public void setLast_login_ip(String last_login_ip) {
+        this.last_login_ip = last_login_ip;
+    }
+
+    public String getLast_login_time() {
+        return last_login_time;
+    }
+
+    public void setLast_login_time(String last_login_time) {
+        this.last_login_time = last_login_time;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public int getLogin_count() {
+        return login_count;
+    }
+
+    public void setLogin_count(int login_count) {
+        this.login_count = login_count;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(String create_time) {
+        this.create_time = create_time;
+    }
+
+    public String getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(String update_time) {
+        this.update_time = update_time;
+    }
+}
---
+++ 0422/3120190971345-朱佳鹏/xihua_demo/src/main/java/com/zlt/service/UserService.java
@@ -0,0 +1,10 @@
+package com.zlt.service;
+
+import com.zlt.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    public List<User> getAllUsers();
+}
